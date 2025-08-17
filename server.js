4c8f3b114dd969e69207c002f102a5a3d499b74d
sushant09112004 <jadhavsushant379@gmail.com>
2025-08-17 21:45:15 +0530

    middleware added

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.use(expresss.json());
 app.use('/api', routes);
 
 //start the server 
+//error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen (port , () =>{
     console.log(`server is running of ${port} `)
